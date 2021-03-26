a07b0919502e4804337d8e8e45e3c52996ee874b
kamal timalsina <kamaltimalsina012@gmail.com>
2021-03-26 19:47:08 +0545

    aa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Printservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
